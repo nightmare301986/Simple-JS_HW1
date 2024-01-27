1415f683b0700ae9661a37e3e3e5aa3fd69a833b
Evgeniy Pakhomov <nightmare23@yandex.ru>
2024-01-27 18:46:16 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+/*let tempCelsius = 15;
+
+let tempFahren = (9/5) * tempCelsius + 32;
+
+console.log("Температура в градусах Цельсия: ", tempCelsius ,"Температура в градусах Фаренгейта: ", tempFahren);*/
+
+let name = "Евгений";
+
+let admin = name;
+
+console.log("Значение переменной admin равно: ", admin);
